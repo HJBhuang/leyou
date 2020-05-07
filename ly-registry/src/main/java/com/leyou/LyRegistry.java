38bbb952b2b035c712fc488e252828e8a8175ca3
hjb <645246861@qq.com>
2020-05-07 17:48:24 +0800

    创建eureka注册中心

---
+++ ly-registry/src/main/java/com/leyou/LyRegistry.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.swing.*;
+
+/**
+ * @author huangjiabao
+ * @data 2020/5/7/0007
+ * @time 17:44:12
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LyRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(LyRegistry.class,args);
+    }
+}
