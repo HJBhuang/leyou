103c7b3d090b544263cfabd55397df5dbf765aa2
hjb <645246861@qq.com>
2020-05-08 09:50:20 +0800

    项目环境搭建

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -0,0 +1,16 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangjiabao
+ * @data 2020/5/7/0007
+ * @time 18:21:17
+ */
+@SpringBootApplication
+public class LyItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemService.class,args);
+    }
+}
