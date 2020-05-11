07dacaf7c6c009a8f4a4518d8c83bd176d762ecf
hjb <645246861@qq.com>
2020-05-11 20:11:41 +0800

    @

--- src/main/java/com/leyou/parent/leyou/LeyouApplication.java
+++ src/main/java/com/leyou/parent/leyou/LeyouApplication.java
@@ -8,7 +8,6 @@ public class LeyouApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LeyouApplication.class, args);
-        System.out.println(args);
     }
 
 }
